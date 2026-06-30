52c767176c5d043932e50eb29372c49b8c13319b
tnation1392 <96146191+tnation1392@users.noreply.github.com>
2026-06-30 16:24:21 -0400

    Added annotations to conftest.py and helpers.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from app.main import app
 from app.db import SessionLocal
 from app.db_models import User, Project, Task
 
-
+#A fixture to clear the database before each test for purity of environment
 @pytest.fixture(autouse=True)
 def clear_databases():
     db = SessionLocal()
@@ -16,7 +16,7 @@ def clear_databases():
     finally:
         db.close()
 
-
+#A fixture to make an HTTP client to test the FastAPI app
 @pytest.fixture
 async def client():
     transport = ASGITransport(app=app)
@@ -24,7 +24,7 @@ async def client():
     async with AsyncClient(transport=transport, base_url="http://test") as ac:
         yield ac
 
-
+#A fixture to create and return a test use with an api_key
 @pytest.fixture
 async def auth_user(client):
     response = await client.post("/users/", json={"name": "Test User"})
@@ -32,7 +32,7 @@ async def auth_user(client):
 
     return {"user_id": data["id"], "api_key": data["api_key"]}
 
-
+#A fixture to build an X-API-key header dictionary from the auth_user fixture
 @pytest.fixture
 async def auth_headers(auth_user):
     return {"x-api-key": auth_user["api_key"]}
